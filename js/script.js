59ef036e873fa8224d632402ebaf2ff1e5cebf55
stvarvara <123358404+stvarvara@users.noreply.github.com>
2024-03-13 09:51:54 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 class Pokemon {
     static all_pokemons = {};
+
     constructor(id, nom, form, attack, defense, stamina) {
         this._pokemonId = id;
         this._pokemonName = nom;
@@ -8,50 +9,55 @@ class Pokemon {
         this._baseDefense = defense;
         this._baseStamina = stamina;
     }
+
     get pokemonId() {
         return this._pokemonId;
     }
+
     get pokemonName() {
         return this._pokemonName;
     }
+
     get form() {
         return this._form;
     }
+
     get baseAttack() {
         return this._baseAttack;
     }
+
     get baseDefense() {
         return this._baseDefense;
     }
+
     get baseStamina() {
         return this._baseStamina;
     }
 
     toString() {
-        return `Pokemon ID: ${this.pokemonId}, Name: ${this.pokemonName}, Form: ${this.form}, Attack: ${this.baseAttack}, Defense: ${this.baseDefense}, Stamina: ${this.baseStamina}\n`;
+        return `ID: ${this.pokemonId}, N: ${this.pokemonName}, F: ${this.form}, A: ${this.baseAttack}, D: ${this.baseDefense}, S: ${this.baseStamina}\n`;
     }
-}
 
-function import_pokemon(pokemon) {
-    // Parcourir les données et créer des objets Pokemon
-    for (let data of pokemon) {
-        // la forme du Pokemon est "Normal"
-        if (data.form === "Normal") {
-            const pokemonObj = new Pokemon(
-                data.pokemon_id,
-                data.pokemon_name,
-                data.form,
-                data.base_attack,
-                data.base_defense,
-                data.base_stamina
-            );
-            Pokemon.all_pokemons[data.pokemon_id] = pokemonObj;
+
+    static importPokemon(pokemon) {
+        for (let data of pokemon) {
+            if (data.form === "Normal") {
+                const pokemonObj = new Pokemon(
+                    data.pokemon_id,
+                    data.pokemon_name,
+                    data.form,
+                    data.base_attack,
+                    data.base_defense,
+                    data.base_stamina,
+                );
+                Pokemon.all_pokemons[data.pokemon_id] = pokemonObj;
+            }
         }
+        return Pokemon.all_pokemons;
     }
-
-    return Pokemon.all_pokemons;
 }
 
-const all_pokemons = import_pokemon(pokemon);
+Pokemon.importPokemon(pokemon);
 
-console.log(Pokemon.all_pokemons);
+let bulbasaur = Pokemon.all_pokemons[1]; 
+console.log(bulbasaur);
