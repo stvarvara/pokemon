2d313e8856b48c99c82774b79a49f703dc2dcb49
stvarvara <123358404+stvarvara@users.noreply.github.com>
2024-03-15 13:45:21 +0100

    Add files via upload

---
+++ html/test/test.js
@@ -0,0 +1,4 @@
+console.log(Pokemon.all_pokemons[1].toString());
+console.log(Pokemon.all_pokemons[2].toString());
+console.log(Type.all_types["Grass"].effectiveness);
+console.log(Type.all_types["Fire"].effectiveness); 
