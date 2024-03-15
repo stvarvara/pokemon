f93d8a0c8be8fd5453defec98660a5e2336e4fff
stvarvara <123358404+stvarvara@users.noreply.github.com>
2024-03-15 12:57:38 +0000

    tests

--- html/test/test.js
+++ html/test/test.js
@@ -1,4 +1,31 @@
-console.log(Pokemon.all_pokemons[1].toString());
-console.log(Pokemon.all_pokemons[2].toString());
-console.log(Type.all_types["Grass"].effectiveness);
-console.log(Type.all_types["Fire"].effectiveness); 
+///Q1 la liste des Pokémons par type
+function getPokemonsByType(typeName){
+
+}
+///Q2 la liste des Pokémons par attaque
+function getPokemonsByAttack(attackName) {
+    
+}
+///Q3 la liste des attaques par type
+function getPokemonsByAttack(attackName) {
+    
+}
+///Q4 a liste des Pokémons triés par nom dans l’ordre alphabétique
+function sortPokemonByName() {
+    
+}
+
+///Q5 la liste des Pokémons triés dans l’ordre décroissant d’endurance (stamina)
+function sortPokemonByStamina() {
+    
+}
+
+///Q6 t la liste des Pokémons pour lesquels l’attaque choisie est la plus efficace
+function getWeakestEnemies(attack) {
+    
+}
+
+///Q7 la liste des types d’attaque les plus efficaces contre un Pokémon donné
+function getBestAttackTypesForEnemy(name)  {
+    
+}
