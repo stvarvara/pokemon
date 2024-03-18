78cfafe3c1dcfa48543061affa3a69ef21f064f8
stvarvara <123358404+stvarvara@users.noreply.github.com>
2024-03-18 19:57:17 +0100

    Update class_type.js

--- html/data/class_type.js
+++ html/data/class_type.js
@@ -1,11 +1,14 @@
-// class Type pour connaître l’efficacité d’un type d’attaque contre
-// un type de défenseur
+// class Type
 class Type {
-    static all_types = {}; //  l’ensemble des types
+    static all_types = {};
 
     constructor(name) {
         this._name = name;
-        this._effectiveness = type_effectiveness[name] || {}; // l’efficacité de type ou la liste vide s'il n'existe pas dans le fichier
+        this._effectiveness = type_effectiveness[name] || {};
+    }
+
+    toString() {
+        return this._name;
     }
 
     get name() {
@@ -16,7 +19,7 @@ class Type {
         return this._effectiveness;
     }
 
-    toString() {
-        return this.name;
+    getEffectivenessByType(type) {
+        return this._effectiveness[type];
     }
 }
